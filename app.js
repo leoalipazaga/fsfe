34ae2296ccb6f9b94d712b0d13c1b96c987790da
Leonardo Alipazaga <leoalipazaga@Leonardos-MacBook-Pro-2.local>
2024-09-14 15:24:34 -0500

    fix: start server from http instances

--- app.js
+++ app.js
@@ -1,40 +1,39 @@
-const express = require("express")
-const http = require("http")
-const WebSocket = require("ws").Server
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws").Server;
 // const app = http.createServer((req, res) => {
-  // res.write("On the way to being a full snack developer!")
-  // res.end()
+// res.write("On the way to being a full snack developer!")
+// res.end()
 // })
-const app = express()
-const server = http.createServer(app)
-
-app.get("/", (req, res) => {
-  res.sendFile("index.html", {root: __dirname})
-})
+const app = express();
+const server = http.createServer(app);
 
 // server.on("request", app)
 
-const ws = new WebSocket({ server })
+const ws = new WebSocket({ server });
 
-ws.on("connection", res => {
-  const clients = ws.clients.size
-  console.log(`client connected ${clients}`)
-  ws.broadcast(`Current visitors ${clients}`)
-  if(res.readyState === res.OPEN) {
-    res.send("Welcome to my server")
+ws.on("connection", (res) => {
+  const clients = ws.clients.size;
+  console.log(`client connected ${clients}`);
+  ws.broadcast(`Current visitors ${clients}`);
+  if (res.readyState === res.OPEN) {
+    res.send("Welcome to my server");
   }
   res.on("close", () => {
-    console.log("A client has disconnected")
-  })
-}) 
+    console.log("A client has disconnected");
+  });
+});
 
 ws.broadcast = (data) => {
   ws.clients.forEach((client) => {
-    client.send(data)
-  })
-}
+    client.send(data);
+  });
+};
 
-app.listen(3000, () => {
-	console.log("Server started");
-})
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: __dirname });
+});
 
+server.listen(3000, () => {
+  console.log("Server started");
+});
