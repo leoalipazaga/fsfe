16e5341775181a13c1564033eafa841d8cc5bd30
Leo Alipazaga <leonardoalipazagab@gmail.com>
2024-09-14 20:12:31 +0000

    feat: web socket

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
+const express = require("express")
 const http = require("http")
-const app = http.createServer((req, res) => {
-  res.write("On the way to being a full snack developer!")
-  res.end()
+const WebSocket = require("ws").Server
+// const app = http.createServer((req, res) => {
+  // res.write("On the way to being a full snack developer!")
+  // res.end()
+// })
+const app = express()
+const server = http.createServer(app)
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html", {root: __dirname})
 })
 
-app.listen(3000)
+// server.on("request", app)
 
-console.log("listening")
+const ws = new WebSocket({ server })
+
+ws.on("connection", res => {
+  const clients = ws.clients.size
+  console.log(`client connected ${clients}`)
+  ws.broadcast(`Current visitors ${clients}`)
+  if(res.readyState === res.OPEN) {
+    res.send("Welcome to my server")
+  }
+  res.on("close", () => {
+    console.log("A client has disconnected")
+  })
+}) 
+
+ws.broadcast = (data) => {
+  ws.clients.forEach((client) => {
+    client.send(data)
+  })
+}
+
+app.listen(3000, () => {
+	console.log("Server started");
+})
+
