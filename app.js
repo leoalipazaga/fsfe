f793ec5ca55567f01035dc9e23650280b15fd3d2
Leonardo Alipazaga <leoalipazaga@Leonardos-MacBook-Pro-2.local>
2024-09-15 07:47:43 -0500

    feat: add sqlite logging

--- app.js
+++ app.js
@@ -7,8 +7,26 @@ const WebSocket = require("ws").Server;
 // })
 const app = express();
 const server = http.createServer(app);
+const sqlite = require("sqlite3");
+const db = new sqlite.Database(":memory:");
 
 // server.on("request", app)
+
+db.serialize(() => {
+  db.run(`CREATE TABLE IF NOT EXISTS visitors (count INTEGER, time TEXT)`);
+});
+
+const countVisitors = () => {
+  db.each(`SELECT * FROM visitors`, (error, row) => {
+    console.log("row", row);
+  });
+};
+
+const shutdownDB = () => {
+  countVisitors();
+  console.log("shutdow db");
+  db.close();
+};
 
 const ws = new WebSocket({ server });
 
@@ -19,6 +37,9 @@ ws.on("connection", (res) => {
   if (res.readyState === res.OPEN) {
     res.send("Welcome to my server");
   }
+  db.run(
+    `INSERT INTO visitors (count, time) VALUES (${clients}, datetime('now'))`,
+  );
   res.on("close", () => {
     console.log("A client has disconnected");
   });
@@ -36,4 +57,13 @@ app.get("/", (req, res) => {
 
 server.listen(3000, () => {
   console.log("Server started");
+});
+
+process.on("SIGINT", () => {
+  ws.clients.forEach((client) => {
+    client.close();
+  });
+  server.close(() => {
+    shutdownDB();
+  });
 });
